cce0624b3fe95ebe3bf729f2b2106420b6920f0a
oxoagency <98583124+oxoagency@users.noreply.github.com>
2022-12-17 09:01:12 +0400

    Added Elementor Init

--- video-fallback-fixer/core/includes/assets/js/hide-video.js
+++ video-fallback-fixer/core/includes/assets/js/hide-video.js
@@ -1,26 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let videoContainer = document.querySelector('.background-video-fix video');
-    videoContainer.style.opacity = "0";
-    detectPowerSavingMode();
-    detectFrameRate();
-    detectPowerSavingMode().then((result) => {
-        if (result == true) {
-            videoContainer.style.opacity = "0";
-            triggerVideoPlay()
-        } else {
-            videoContainer.style.opacity = "1";
-        }
-    });
-    function triggerVideoPlay() {
-        let videoPlayer = document.querySelector('.background-video-fix video');
-        jQuery('body').on('click touchstart', function () {
-            if (videoPlayer.playing) {
-                
-            }
-            else {
-                videoPlayer.play();
-                videoPlayer.style.opacity = "1";
+// When the window was loaded
+window.addEventListener('DOMContentLoaded', () => {
+
+    // When Elementor was loaded
+    window.addEventListener('elementor/frontend/init', () => {
+
+        let videoContainer = document.querySelector('.background-video-fix video');
+        videoContainer.style.opacity = "0";
+        detectPowerSavingMode();
+        detectFrameRate();
+        detectPowerSavingMode().then((result) => {
+            if (result == true) {
+                videoContainer.style.opacity = "0";
+                triggerVideoPlay()
+            } else {
+                videoContainer.style.opacity = "1";
             }
         });
-    }    
+
+        function triggerVideoPlay() {
+            let videoPlayer = document.querySelector('.background-video-fix video');
+            jQuery('body').on('click touchstart', function () {
+                if (videoPlayer.playing) {
+
+                } else {
+                    videoPlayer.play();
+                    videoPlayer.style.opacity = "1";
+                }
+            });
+        }
+
+    });
+
 });
