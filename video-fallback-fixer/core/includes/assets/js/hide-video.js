99d41dc5dda484a8b3a9ae10c06bd357bc253b71
oxoagency <98583124+oxoagency@users.noreply.github.com>
2022-12-17 08:37:36 +0400

    Added play on user interaction

--- video-fallback-fixer/core/includes/assets/js/hide-video.js
+++ video-fallback-fixer/core/includes/assets/js/hide-video.js
@@ -6,8 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
     detectPowerSavingMode().then((result) => {
         if (result == true) {
             videoContainer.style.opacity = "0";
+            triggerVideoPlay()
         } else {
             videoContainer.style.opacity = "1";
         }
     });
+    function triggerVideoPlay() {
+        jQuery('body').on('click touchstart', function () {
+            let videoContainer = document.querySelector('.background-video-fix video');
+            if (videoContainer.playing) {
+                
+            }
+            else {
+                videoContainer.play();
+                videoContainer.style.opacity = "1";
+            }
+        });
+    }    
 });
